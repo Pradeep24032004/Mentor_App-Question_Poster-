43604f52421439c7cafc0ae46de25b463401bf4c
pradeep <118010705+Pradeep24032004@users.noreply.github.com>
2024-06-07 09:59:35 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter} from "react-router-dom";
 import App from './App.js';
-//import SignUp from './pages/signup/SignUp';
 import SignIn from './components/SignIn.jsx';
 import UserHome from './components/UserHome.jsx';
 import MentorSignUp from './components/MentorSignUp.jsx';
@@ -44,8 +43,4 @@ root.render(
   <RouterProvider router = {router} />
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
