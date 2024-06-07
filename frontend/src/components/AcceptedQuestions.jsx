5f26fba9bc41119c1257ae185ef26d2f174f97ce
pradeep <118010705+Pradeep24032004@users.noreply.github.com>
2024-06-07 10:00:14 +0530

    Update AcceptedQuestions.jsx

--- frontend/src/components/AcceptedQuestions.jsx
+++ frontend/src/components/AcceptedQuestions.jsx
@@ -1,9 +1,6 @@
 import React, {useState, useEffect} from "react";
-//import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-//import DisplayQuestion from "./DisplayQuestion.jsx";
 import MentorResponse from "./MentorResponse.jsx";
-//import Question from "./Question.jsx";
 function AcceptedQuestions (){
     const [user, setUser] = useState(null);
     //const navigate = useNavigate();
